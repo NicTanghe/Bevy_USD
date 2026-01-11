4b1c4095f341f4dbfd3e02a63987928d07ab0ebe
Ni <nic.tanghe@gmail.com>
2026-01-11 02:24:15 +0100

    added funciton to handle quat rotation calculations in lib

--- src/open_rs_loader.rs
+++ src/open_rs_loader.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use glam::{DMat4, Mat4, Quat, Vec3};
+use glam::{DMat4, DQuat, DVec3, Mat4, Quat, Vec3};
 use openusd_rs::{
     gf::{self, Matrix4d},
     tf::Token,
@@ -64,8 +64,199 @@ pub struct StageData {
 }
 
 // -------- Local transform --------
+#[derive(Clone, Copy)]
+enum XformOpType {
+    // Scalar
+    TranslateX,
+    TranslateY,
+    TranslateZ,
+    // Vec3
+    Translate,
+    // Scalar
+    ScaleX,
+    ScaleY,
+    ScaleZ,
+    // Vec3
+    Scale,
+    // Scalar
+    RotateX,
+    RotateY,
+    RotateZ,
+    // Vec3
+    RotateXYZ,
+    RotateXZY,
+    RotateYXZ,
+    RotateYZX,
+    RotateZXY,
+    RotateZYX,
+    // Quat
+    Orient,
+    // Matrix4
+    Transform,
+}
+
+impl TryFrom<&str> for XformOpType {
+    type Error = ();
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        Ok(match s {
+            "translateX" => XformOpType::TranslateX,
+            "translateY" => XformOpType::TranslateY,
+            "translateZ" => XformOpType::TranslateZ,
+            "translate" => XformOpType::Translate,
+            "scaleX" => XformOpType::ScaleX,
+            "scaleY" => XformOpType::ScaleY,
+            "scaleZ" => XformOpType::ScaleZ,
+            "scale" => XformOpType::Scale,
+            "rotateX" => XformOpType::RotateX,
+            "rotateY" => XformOpType::RotateY,
+            "rotateZ" => XformOpType::RotateZ,
+            "rotateXYZ" => XformOpType::RotateXYZ,
+            "rotateXZY" => XformOpType::RotateXZY,
+            "rotateYXZ" => XformOpType::RotateYXZ,
+            "rotateYZX" => XformOpType::RotateYZX,
+            "rotateZXY" => XformOpType::RotateZXY,
+            "rotateZYX" => XformOpType::RotateZYX,
+            "orient" => XformOpType::Orient,
+            "transform" => XformOpType::Transform,
+            _ => return Err(()),
+        })
+    }
+}
+
+fn get_op_transform_degrees(op_type: XformOpType, value: vt::Value, is_inverse: bool) -> Option<DMat4> {
+    use XformOpType::*;
+
+    let get_scalar = || -> Option<f64> {
+        value
+            .get::<f64>()
+            .or_else(|| value.get::<f32>().map(|v| v.into()))
+    };
+
+    let get_vec3 = || -> Option<DVec3> {
+        value
+            .get::<gf::Vec3d>()
+            .or_else(|| value.get::<gf::Vec3f>().map(|v| v.into()))
+            .or_else(|| value.get::<gf::Vec3h>().map(|v| v.into()))
+            .map(|v| DVec3::new(v.x, v.y, v.z))
+    };
+
+    let get_quat = || -> Option<DQuat> {
+        value
+            .get::<gf::Quatd>()
+            .or_else(|| value.get::<gf::Quatf>().map(|v| v.into()))
+            .or_else(|| value.get::<gf::Quath>().map(|v| v.into()))
+            .map(|v| DQuat::from_xyzw(v.i, v.j, v.k, v.w))
+    };
+
+    Some(match op_type {
+        TranslateX if is_inverse => DMat4::from_translation(DVec3::new(-get_scalar()?, 0.0, 0.0)),
+        TranslateY if is_inverse => DMat4::from_translation(DVec3::new(0.0, -get_scalar()?, 0.0)),
+        TranslateZ if is_inverse => DMat4::from_translation(DVec3::new(0.0, 0.0, -get_scalar()?)),
+        Translate if is_inverse => DMat4::from_translation(-get_vec3()?),
+
+        TranslateX => DMat4::from_translation(DVec3::new(get_scalar()?, 0.0, 0.0)),
+        TranslateY => DMat4::from_translation(DVec3::new(0.0, get_scalar()?, 0.0)),
+        TranslateZ => DMat4::from_translation(DVec3::new(0.0, 0.0, get_scalar()?)),
+        Translate => DMat4::from_translation(get_vec3()?),
+
+        ScaleX if is_inverse => DMat4::from_scale(DVec3::new(1.0 / get_scalar()?, 1.0, 1.0)),
+        ScaleY if is_inverse => DMat4::from_scale(DVec3::new(1.0, 1.0 / get_scalar()?, 1.0)),
+        ScaleZ if is_inverse => DMat4::from_scale(DVec3::new(1.0, 1.0, 1.0 / get_scalar()?)),
+        Scale if is_inverse => DMat4::from_scale(1.0 / get_vec3()?),
+
+        ScaleX => DMat4::from_scale(DVec3::new(get_scalar()?, 1.0, 1.0)),
+        ScaleY => DMat4::from_scale(DVec3::new(1.0, get_scalar()?, 1.0)),
+        ScaleZ => DMat4::from_scale(DVec3::new(1.0, 1.0, get_scalar()?)),
+        Scale => DMat4::from_scale(get_vec3()?),
+
+        RotateX | RotateY | RotateZ => {
+            let angle = get_scalar()?.to_radians();
+            let angle = if is_inverse { -angle } else { angle };
+            match op_type {
+                RotateX => DMat4::from_rotation_x(angle),
+                RotateY => DMat4::from_rotation_y(angle),
+                RotateZ => DMat4::from_rotation_z(angle),
+                _ => unreachable!(),
+            }
+        }
+
+        RotateXYZ | RotateXZY | RotateYXZ | RotateYZX | RotateZXY | RotateZYX => {
+            let vec = get_vec3()?;
+            let vec = if is_inverse { -vec } else { vec };
+            let vec = DVec3::new(vec.x.to_radians(), vec.y.to_radians(), vec.z.to_radians());
+
+            let rot_x = DQuat::from_axis_angle(DVec3::X, vec.x);
+            let rot_y = DQuat::from_axis_angle(DVec3::Y, vec.y);
+            let rot_z = DQuat::from_axis_angle(DVec3::Z, vec.z);
+
+            let rot = match op_type {
+                RotateXYZ if is_inverse => rot_z * rot_y * rot_x,
+                RotateXZY if is_inverse => rot_y * rot_z * rot_x,
+                RotateYXZ if is_inverse => rot_z * rot_x * rot_y,
+                RotateYZX if is_inverse => rot_x * rot_z * rot_y,
+                RotateZXY if is_inverse => rot_y * rot_x * rot_z,
+                RotateZYX if is_inverse => rot_x * rot_y * rot_z,
+
+                RotateXYZ => rot_x * rot_y * rot_z,
+                RotateXZY => rot_x * rot_z * rot_y,
+                RotateYXZ => rot_y * rot_x * rot_z,
+                RotateYZX => rot_y * rot_z * rot_x,
+                RotateZXY => rot_z * rot_x * rot_y,
+                RotateZYX => rot_z * rot_y * rot_x,
+                _ => unreachable!(),
+            };
+
+            DMat4::from_quat(rot)
+        }
+        Orient => DMat4::from_quat(if is_inverse {
+            get_quat()?.inverse()
+        } else {
+            get_quat()?
+        }),
+        Transform => {
+            let mat = value.get::<gf::Matrix4d>()?;
+            // USD uses row-major, glam uses column-major, so transpose here
+            let mat = DMat4::from_cols_array_2d(&mat.data).transpose();
+            if is_inverse { mat.inverse() } else { mat }
+        }
+    })
+}
+
+fn get_local_matrix_degrees(prim: &usd::Prim) -> Option<Matrix4d> {
+    if !prim.has_attribute(&usd_geom::TOKENS.xform_op_order) {
+        return None;
+    }
+
+    let op_order = prim
+        .attribute(&usd_geom::TOKENS.xform_op_order)
+        .get::<vt::Array<Token>>();
+
+    let mut transform = DMat4::IDENTITY;
+    for op in op_order.iter().rev() {
+        let (op, is_inverse) = op
+            .as_str()
+            .strip_prefix("!invert!")
+            .map_or((op.as_str(), false), |s| (s, true));
+
+        if op == "resetXformStack" {
+            continue;
+        }
+
+        let op_type = XformOpType::try_from(op.trim_start_matches("xformOp:")).ok();
+        let op_value = prim.attribute(&Token::new(op)).get_value();
+
+        if let (Some(op_type), Some(op_value)) = (op_type, op_value) {
+            if let Some(op_transform) = get_op_transform_degrees(op_type, op_value, is_inverse) {
+                transform *= op_transform;
+            }
+        }
+    }
+
+    Some(dmat4_to_matrix4d(transform))
+}
+
 fn get_local_transform(prim: &usd::Prim) -> Option<Matrix4d> {
-    if let Some(matrix) = usd_geom::XformOp::get_local_matrix(prim) {
+    if let Some(matrix) = get_local_matrix_degrees(prim) {
         return Some(matrix);
     }
 
